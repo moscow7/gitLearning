26e946e47b2a90d26504a761cff283cd731286c3
moscow7 <62070914+moscow7@users.noreply.github.com>
2020-05-21 03:57:08 +0200

    Update Practice.java

--- src/tests/Practice.java
+++ src/tests/Practice.java
@@ -4,6 +4,8 @@ public class Practice {
     public static void main(String[] args) {
         //it is second part of learning
         System.out.println("hello tuesday ");
+        
+        //this line is written by GitHub 
 
 
     }
