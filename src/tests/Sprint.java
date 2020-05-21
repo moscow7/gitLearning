f1abb6a34a8a6b4df176f3bef0be051cf4cff100
moscow7 <mr.mdaler@gmail.com>
2020-05-21 02:09:04 +0200

    add new spring

--- src/tests/Sprint.java
+++ src/tests/Sprint.java
@@ -4,6 +4,8 @@ public class Sprint {
     public static void main(String[] args) {
 
         System.out.println("write some cods...");
+        System.out.println("good bay");
         //proverka
+
     }
 }
