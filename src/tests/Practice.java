533a2491af0ac37b6ba3080c53bed1d7de768cc6
moscow7 <mr.mdaler@gmail.com>
2020-05-21 03:50:50 +0200

    NEW CLASS

---
+++ src/tests/Practice.java
@@ -0,0 +1,10 @@
+package tests;
+
+public class Practice {
+    public static void main(String[] args) {
+        //it is second part of learning
+        System.out.println("hello tuesday ");
+
+
+    }
+}
