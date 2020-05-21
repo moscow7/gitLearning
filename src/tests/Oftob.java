0dad1db76c31a23e3bce8a5c3403569b779f98bc
moscow7 <mr.mdaler@gmail.com>
2020-05-21 02:39:18 +0200

    new class oftob

---
+++ src/tests/Oftob.java
@@ -0,0 +1,9 @@
+package tests;
+
+public class Oftob {
+    public static void main(String[] args) {
+        System.out.println("winter to add something");
+        //i wont to see this line on github
+
+    }
+}
